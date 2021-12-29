e867d7c666f05235c7e65df08a469e5603c30538
dariy-kordiyak <dariy.kordiyak@globallogic.com>
2021-12-29 22:09:26 +0200

    Add Templates folder container

--- Templates/Basic/model/basic_server_response.dart
+++ Templates/Basic/model/basic_server_response.dart
@@ -1,19 +1,18 @@
-
-class TemplateServerResponse {
+class BasicServerResponse {
   final int id;
   final String title;
   final String url;
   final String thumbnailUrl;
 
-  const TemplateServerResponse({
+  const BasicServerResponse({
     required this.id,
     required this.title,
     required this.url,
     required this.thumbnailUrl,
   });
 
-  factory TemplateServerResponse.fromJson(Map<String, dynamic> json) {
-    return TemplateServerResponse(
+  factory BasicServerResponse.fromJson(Map<String, dynamic> json) {
+    return BasicServerResponse(
       id: json['id'] as int,
       title: json['title'] as String,
       url: json['url'] as String,
--- Templates/Basic/ui/basic_colors.dart
+++ Templates/Basic/ui/basic_colors.dart
@@ -1,6 +1,6 @@
-
 import 'package:flutter/rendering.dart';
+import 'package:basic_server_response.dart';
 
-class TemplateColors {
+class BasicColors {
   final appTheme = const Color.fromARGB(255, 255, 255, 1);
 }
