86ce56a69bcd5ce35dcc90c51b5c85a2dfdea32a
dariy-kordiyak <dariy.kordiyak@globallogic.com>
2021-12-29 13:50:04 +0200

    Add ModuleParser and ArgumentsParser classes

--- module_generator.py
+++ module_generator.py
@@ -4,42 +4,60 @@ import shutil
 import argparse
 from posixpath import dirname
 
-# Replaces text in the given file from Template text to Module text
-def replaceContents(filePath):
-    f = open(filePath,'r')
-    lines = f.readlines()
-    f.close()
-    f = open(filePath,'w')
-    for line in lines:
-        newline = line.replace(templateName, moduleName)
-        f.write(newline)
-    f.close()
+class ArgumentsParser:
+    __fallbackName = 'Default'
 
-# Module name parser
-parser = argparse.ArgumentParser(description = 'Generates Flutter submodule architecture based on template')
-parser.add_argument('-t', '--Template', help = 'Name of template to generate from')
-parser.add_argument('-n', '--Name', help = 'Name of generated module')
-args = parser.parse_args()
+    def parseUserInput(self):
+        parser = argparse.ArgumentParser(description = 'Generates Flutter submodule architecture based on template')
+        parser.add_argument('-t', '--Template', help = 'Name of template to generate from')
+        parser.add_argument('-n', '--Name', help = 'Name of generated module')
+        args = parser.parse_args()
+        self.templateName = args.Template if args.Template else self.__fallbackName
+        self.templatePath = os.getcwd() + '/' + self.templateName
+        self.moduleName = args.Name if args.Name else self.__fallbackName
 
-# Constants
-extension = '.dart'
-fallbackName = 'Default'
-templateName = args.Template if args.Template else fallbackName
-templatePath = os.getcwd() + '/' + templateName
-moduleName = args.Name if args.Name else fallbackName
-generatedModuleName = 'gen_' + moduleName
+
+class ModuleParser:
+    __parsedExtension = '.dart'
+
+    def __init__(self, templateName, moduleName, generatedModuleName):
+        self.templateName = templateName
+        self.moduleName = moduleName
+        self.generatedModuleName = generatedModuleName
+
+     # Traverse generated folder and replace template content of files with given extension
+    def traverseAndParseFiles(self):
+        for root, dirs, files in os.walk(self.generatedModuleName):
+            for fileName in files:
+               if pathlib.Path(fileName).suffix == self.__parsedExtension:
+                  filePath = os.path.join(root, fileName)
+                  renamedFilePath = filePath.replace(self.templateName.lower(), self.moduleName.lower())
+                  self.__replaceContents(filePath)
+                  os.rename(filePath, renamedFilePath)
+
+    # Replace text in the given file from Template text to Module text
+    def __replaceContents(self, filePath):
+        f = open(filePath,'r')
+        lines = f.readlines()
+        f.close()
+        f = open(filePath,'w')
+        for line in lines:
+            newline = line.replace(self.templateName, self.moduleName)
+            f.write(newline)
+        f.close()
+
+# Parse user input arguments and prepare names / paths
+argumentsParser = ArgumentsParser()
+argumentsParser.parseUserInput()
+generatedModuleName = 'gen'
 
 # Copy entire folder tree structure to the generated folder with module name
-shutil.copytree(templatePath, generatedModuleName)
+shutil.copytree(argumentsParser.templatePath, generatedModuleName)
 
-# Traverse generated folder and replace template content of files with given extension
-for root, dirs, files in os.walk(generatedModuleName):
-    for fileName in files:
-        if pathlib.Path(fileName).suffix == extension:
-            filePath = os.path.join(root, fileName)
-            renamedFilePath = filePath.replace(templateName.lower(), moduleName.lower())
-            replaceContents(filePath)
-            os.rename(filePath, renamedFilePath)
+# Parse copied template tree and rename files / contents to module name
+moduleParser = ModuleParser(argumentsParser.templateName, argumentsParser.moduleName, generatedModuleName)
+moduleParser.traverseAndParseFiles()
+
 
 
        
