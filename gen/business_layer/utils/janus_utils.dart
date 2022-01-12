283e1dbfc83dda24cd96aed851e1818171e313aa
dariy-kordiyak <dariy.kordiyak@globallogic.com>
2022-01-12 11:38:59 +0200

    Remove gen folder

--- gen/business_layer/utils/janus_utils.dart
+++
@@ -1,5 +0,0 @@
-class JanusUtils {
-  static final Map<String, Map<String, String>> data = {
-    'key': {'name': 'janus name', 'description': 'janus description'},
-  };
-}
--- gen/service_layer/main_service/main_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:janus/business_layer/models/janus_model.dart';
-
-abstract class MainService {
-  Future addData(List<JanusModel> data);
-
-  Future<List<JanusModel>> getData();
-}
