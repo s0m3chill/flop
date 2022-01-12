0dcf8b0d4f4bed3ad1697a044773abd587550ab1
dariy-kordiyak <dariy.kordiyak@globallogic.com>
2022-01-12 11:39:47 +0200

    Remove temp folder

--- Templates/Template/business_layer/utils/template_utils.dart
+++
@@ -1,5 +0,0 @@
-class TemplateUtils {
-  static final Map<String, Map<String, String>> data = {
-    'key': {'name': 'template name', 'description': 'template description'},
-  };
-}
--- Templates/Template/service_layer/main_service/main_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:template/business_layer/models/template_model.dart';
-
-abstract class MainService {
-  Future addData(List<TemplateModel> data);
-
-  Future<List<TemplateModel>> getData();
-}
