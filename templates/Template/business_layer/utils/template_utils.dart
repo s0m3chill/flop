dbed848175b58a63e63a4f49f31ac265baa388cb
dariy-kordiyak <dariy.kordiyak@globallogic.com>
2022-01-12 11:40:36 +0200

    Add templates lowercase folder

---
+++ templates/Template/business_layer/utils/template_utils.dart
@@ -0,0 +1,5 @@
+class TemplateUtils {
+  static final Map<String, Map<String, String>> data = {
+    'key': {'name': 'template name', 'description': 'template description'},
+  };
+}
---
+++ templates/Template/service_layer/main_service/main_service_implementation.dart
@@ -0,0 +1,16 @@
+import 'package:template/business_layer/models/template_model.dart';
+import 'package:template/service_layer/main_service/main_service.dart';
+
+class MainServiceImpl implements MainService {
+  @override
+  Future addData(List<TemplateModel> data) {
+    // TODO: implement addData
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<TemplateModel>> getData() {
+    // TODO: implement getData
+    throw UnimplementedError();
+  }
+}
