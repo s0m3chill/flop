15359d2181608b2bebfc9fce7d3965d4902e0f45
dariy-kordiyak <dariy.kordiyak@globallogic.com>
2022-01-26 21:04:09 +0200

    Refactor unit test structure

--- templates/application/test/test_driver/application_integration_test.dart
+++ templates/application/test/test_driver/application_integration_test.dart
@@ -1,7 +1,7 @@
 import 'package:application/main.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
+import 'package:test_driver/test_driver.dart';
 
 void main() {
   group('Testing full app flow', () {
---
+++ templates/application/test/test_driver/driver.dart
@@ -0,0 +1,3 @@
+import 'package:test_driver/integration_test_driver.dart';
+
+Future<void> main() => integrationDriver();
